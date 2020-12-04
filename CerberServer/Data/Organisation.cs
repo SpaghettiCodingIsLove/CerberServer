9a29140518939b6e3329f090831622a30b8f1bc5
Adam Wolszleger <43927273+sylfero@users.noreply.github.com>
2020-12-04 23:45:31 +0100

    Dodanie połączenia z bazą danych

---
+++ CerberServer/Data/Organisation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CerberServer.Data
+{
+    public partial class Organisation
+    {
+        public Organisation()
+        {
+            Operators = new HashSet<Operator>();
+            Users = new HashSet<User>();
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string OrganisationKey { get; set; }
+        public string Logo { get; set; }
+        public bool Active { get; set; }
+
+        public virtual ICollection<Operator> Operators { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
