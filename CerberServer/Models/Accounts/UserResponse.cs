cc9e6d9462516292115575a39c16d797c35bc11f
Adam Wolszleger <43927273+sylfero@users.noreply.github.com>
2021-01-12 14:51:23 +0100

    Dodano zapytania o organizacje

---
+++ CerberServer/Models/Accounts/UserResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CerberServer.Models.Accounts
+{
+    public class UserResponse
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Online { get; set; }
+    }
+}
